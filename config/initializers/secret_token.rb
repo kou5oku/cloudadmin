b7870479f6c4ae6855b6c568b6003babdaf7816f
unknown <kou5oku@ClubFrog.(none)>
2012-02-22 14:19:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudadmin::Application.config.secret_token = 'dfd47888ee130eec3953d98c1f4b165e919c0751e505a3d2be1856302e430cea587a68c0d5c2afefaba35f979c3358493baf9399243e82b308dc3ae81ffbe2e5'
