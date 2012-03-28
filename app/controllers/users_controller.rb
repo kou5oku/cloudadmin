67b148629caf3804c31d0780345e1a607693d95d
kou5oku <kou5oku@gmail.com>
2012-03-27 19:04:41 -0700

    add admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-    before_filter :signed_in_user, only: [:index, :edit, :update]
-    before_filter :correct_user,   only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
   def show
     @user = User.find(params[:id])
   end
@@ -37,6 +39,12 @@ def update
     end
   end
 
+ def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
   private
 
     def signed_in_user
@@ -49,5 +57,9 @@ def update
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
