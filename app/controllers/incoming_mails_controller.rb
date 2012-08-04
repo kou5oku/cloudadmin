46d72539fd8f5556b8c9c4ce5c37d004d7f8d2a5
Lloyd Kalicki <lloyd@kalicki.us>
2012-08-04 11:08:23 -0700

    switch to JSON for incoming emails

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,14 +1,16 @@
 class IncomingMailsController < ApplicationController    
-  require 'mail'
   skip_before_filter :verify_authenticity_token
   
   def create
-    message = Mail.new(params[:message])
-    Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
-    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
+    Rails.logger.info params[:headers][:subject]
+    Rails.logger.info params[:plain]
+    Rails.logger.info params[:html]
 
     # Do some other stuff with the mail message
-
-    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+    if params[:envelope][:from] != 'expected_user@example.com'
+      render :text => 'success', :status => 200
+    else
+      render :text => 'Unknown user', :status => 404 # 404 would reject the mail
+    end
   end
 end
