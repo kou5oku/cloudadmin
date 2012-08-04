404aaed135f88cd2c8c60aeba12e28e7e8924c97
Lloyd Kalicki <lloyd@kalicki.us>
2012-08-04 10:40:32 -0700

    add regular style cloudmailin

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,16 +1,14 @@
 class IncomingMailsController < ApplicationController    
+  require 'mail'
   skip_before_filter :verify_authenticity_token
   
   def create
-    Rails.logger.info params[:headers][:subject]
-    Rails.logger.info params[:plain]
-    Rails.logger.info params[:html]
+    message = Mail.new(params[:message])
+    Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
+    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
 
     # Do some other stuff with the mail message
-    if params[:envelope][:from] != 'expected_user@example.com'
-      render :text => 'success', :status => 200
-    else
-      render :text => 'Unknown user', :status => 404 # 404 would reject the mail
-    end
+
+    render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
 end
