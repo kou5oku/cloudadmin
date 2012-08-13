55596b1563450837e5d1b63746644d7ca4ce7135
Lloyd Kalicki <kou5oku@gmail.com>
2012-08-13 16:21:37 -0700

    route for new server view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Cloudadmin::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :incoming_mails
+  resources :servers
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
